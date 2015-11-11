8509fba2ffbce48a171f79787a5388c3a0f211ed
razor <razor@MacBook-de-razor.local>
2015-11-11 18:58:09 +0100

    icon and go library begining

--- MyAuth/MyAuth/ViewController.h
+++ MyAuth/MyAuth/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <AVCaptureMetadataOutputObjectsDelegate>
 
+@property (weak, nonatomic) IBOutlet UIView *viewPreview;
 
 @end
 
--- MyAuth/MyAuth/ViewController.m
+++ MyAuth/MyAuth/ViewController.m
@@ -7,21 +7,90 @@
 //
 
 #import "ViewController.h"
+#import <Gomobiletest/Gomobiletest.h>
 
 @interface ViewController ()
 
+@property (nonatomic) BOOL isReading;
+
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+-(BOOL)startReading;
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self startReading];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(BOOL)startReading
+{
+    NSError *error;
+    
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    if (!input) {
+        NSLog(@"%@", [error localizedDescription]);
+        return NO;
+    }
+    
+    _captureSession = [[AVCaptureSession alloc] init];
+    [_captureSession addInput:input];
+    
+    AVCaptureMetadataOutput *captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [_captureSession addOutput:captureMetadataOutput];
+    
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetadataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeQRCode]];
+    
+    _videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    [_videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [_videoPreviewLayer setFrame:_viewPreview.layer.bounds];
+    [_viewPreview.layer addSublayer:_videoPreviewLayer];
+    
+    [_captureSession startRunning];
+    
+    return YES;
+}
+
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    if (metadataObjects != nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeQRCode]) {
+            NSLog(@"%@",[metadataObj stringValue]);
+            
+            NSString * result = GoGomobiletestGenearateOtpFromURL([metadataObj stringValue]);
+            NSLog(@"%@",result);
+            //
+//            NSString *pouet = @"otpauth://totp/user@host.com?secret=JBSWY3DPEHPK3PXP";
+//            NSURL *secreturl = [NSURL URLWithString:pouet];
+//            
+//            secreturl.scheme;// "otpauth"
+//            secreturl.host;// "totp"
+//            secreturl.path;// "/usr@host.com"  //relativePath
+//            secreturl.query;// "secret=JBSWY3DPEHPK3PXP"
+            
+            //[_lblStatus performSelectorOnMainThread:@selector(setText:) withObject:[metadataObj stringValue] waitUntilDone:NO];
+            
+            //[self performSelectorOnMainThread:@selector(stopReading) withObject:nil waitUntilDone:NO];
+            //[_bbitemStart performSelectorOnMainThread:@selector(setTitle:) withObject:@"Start!" waitUntilDone:NO];
+            _isReading = NO;
+        }
+    }
+}
+
+
 
 @end
